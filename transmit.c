6ee43ef1824209020ed7633bd1d5da6645a656c9
AarhamH <aarham.haider@gmail.com>
2023-10-17 22:57:24 -0700

    Updated transmiter

--- transmit.c
+++ transmit.c
@@ -1,143 +1,172 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 #include <unistd.h>
-#include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <pthread.h>
+#include "list.h"
 #include "transmit.h"
 
-//AddressInfo structs and endpoints
-static int SocketEndpointValue = -1;
-static struct addrinfo Hints, *ServInfo, *ListTraverse;
-static int AddressValue;
-static int Bytes;
-static char* ListMessage;
+
+// Static 
+static int SocketEndPoint;
+static struct addrinfo *ServInfo;
+static char* Hostname;
+static char* Port;
+static List* LList;
+static pthread_t SenderThread;
+static char* Message;
+struct addrinfo Hints, *ListTraverse;
+
+// what
+int NumBytes;
+int GetAddress;
 
 
-// <summary> Initializes an address info struct hint, which determines the type of socket we are seeking
-//           The family and type are configured to support IPv4 and UDP respectively
-struct addrinfo SetUpHints(struct addrinfo HintsArg)
+// Threads
+static pthread_mutex_t SendAvailableCondMutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t SendAvailableCond = PTHREAD_COND_INITIALIZER;
+
+
+// <summary> This method sets up the hint conditions to create a GetAddress number
+struct addrinfo SetupHints(struct addrinfo HintsArg)
 {
     memset(&HintsArg, 0 ,sizeof (HintsArg));
     HintsArg.ai_family = AF_INET;
     HintsArg.ai_socktype = SOCK_DGRAM;
+    
     return HintsArg;
 }
 
 
-// <summary> Sets up a potential socket. This does this by traversing through ServInfo's Linked List based
-//           on its requirements (family, type, etc.). Returns socket identity value as an int
-int SetUpSocket(int SocketArg)
+// <summary> This method finds an available socket and sets the endpoint. Returns -1 by default if none are found.
+int SetupSocket(int Fd)
 {
-    ListTraverse = ServInfo;
-    while (ListTraverse != NULL)
-    {
-        SocketArg = socket(ListTraverse->ai_family, ListTraverse->ai_socktype, ListTraverse->ai_protocol);
-    
-        if (SocketArg == -1)
-        {
+    for (ListTraverse = ServInfo; ListTraverse != NULL; ListTraverse = ListTraverse->ai_next) {
+        Fd = socket(ListTraverse->ai_family, ListTraverse->ai_socktype, ListTraverse->ai_protocol);
+
+        if (Fd == -1) {
             perror("sender: socket() error");
-            ListTraverse = ListTraverse->ai_next; // Move to the next addrinfo structure
             continue;
         }
-    
         break;
     }
-    
-    return SocketArg;
+    return Fd;
 }
 
-// <summary> Initializes all of the necessary components, such as hints, GAI, and Socket and creates and instance
-//           of the sender. After setup, sender unloads (dequeues) all of the messages from the list arg
-void SetupUDPSender(char* hostname, char* port, List* list)
+// <summary> This method trims the messages one by one until none is left. Called by SetupUnload()
+void* UnloadMessages()
 {
-    // Initializing Hints
-    Hints = SetUpHints(Hints);
-
-    if (AddressValue = getaddrinfo(hostname, port, &Hints, &ServInfo) != 0)
-    {
-        fprintf(stderr, "Error: %s\n", gai_strerror(AddressValue));
-        return;
-    }
-
-    SocketEndpointValue = SetUpSocket(SocketEndpointValue);
-    // Error checking if no sockets are binded
-    if (ListTraverse == NULL)
-    {
-        fprintf(stderr, "Error: Failed to create socket in Sender");
-        return;
-    }
-    
-    // Loop to dequeue list every time and send it over via socket; checking if the bytes are valid
-    while(1)
-    {
-        if(List_count(list) > 0)
+        int Iteration = 0;
+        int ListCount;
+        
+        while ((ListCount = List_lockedCount(LList)) != 0) 
         {
-              ListMessage = (char*)List_trim(list);
-    
-              if((Bytes = sendto(SocketEndpointValue, ListMessage, strlen(ListMessage), 0, ListTraverse->ai_addr, ListTraverse->ai_addrlen)) == -1)
-              {
-                perror("Error: Unable to send message");
-                return;
-              }
-              printf("Succesfully Sent Bytes: %d\n", Bytes);
-              printf("Sent message: %s\n", ListMessage);
-              printf("Sent to address: %s\n", inet_ntoa(((struct sockaddr_in*)ListTraverse->ai_addr)->sin_addr));
-              printf("Sent to port: %d\n", ntohs(((struct sockaddr_in*)ListTraverse->ai_addr)->sin_port));
+            Iteration++;
+
+            // Getting message from list
+            Message = List_output(LList);
+
+            if (Message == NULL) {
+                fprintf(stderr, "sender: dequeue error, queue empty.\n");
+                break;
+            }
+
+            // Sending
+            NumBytes = sendto(SocketEndPoint, Message, strlen(Message), 0, ListTraverse->ai_addr, ListTraverse->ai_addrlen);
+
+            // Check for exit code
+            // Ends the thread if exit code is passed in the first iteration of the read
+            if (!strcmp(Message, "!\n") && Iteration == 1) {
+                free(Message);
+                Message = NULL;
+                return NULL;
+            }
+
+            // De-allocating message
+            free(Message);
+            Message = NULL;
+
+            // Error checking recvfrom
+            if (NumBytes == -1) {
+                perror("sender: sendto error");
+                exit(-1);
+            }
         }
+        return NULL;
+}
+
+// <summary> This method sets up a semaphore, if wait condition is lifted, calls UnloadMessages() to send all messages along socket
+static void* SenderUnload() {
+    
+    Hints = SetupHints(Hints);
+
+    if ((GetAddress = getaddrinfo(Hostname, Port, &Hints, &ServInfo)) != 0) {
+        fprintf(stderr, "sender: getaddrinfo error: %s\n", gai_strerror(GetAddress));
+        exit(-1);
+    }
+
+    SocketEndPoint = SetupSocket(SocketEndPoint);
+
+    if (ListTraverse == NULL) {
+        fprintf(stderr, "sender: failed to create socket");
+        exit(-1);
+    }
+
+    while (1) {
+        pthread_mutex_lock(&SendAvailableCondMutex);
+        {
+          pthread_cond_wait(&SendAvailableCond, &SendAvailableCondMutex);
+        }
+        pthread_mutex_unlock(&SendAvailableCondMutex);
+        
+        UnloadMessages();
 
     }
+    return NULL;
+}
+
+// <summary> This method is called by another thread to say that it is ready
+void senderSignaller() {
+    pthread_mutex_lock(&SendAvailableCondMutex);
+    {
+      pthread_cond_signal(&SendAvailableCond);
+    }
+    pthread_mutex_unlock(&SendAvailableCondMutex);
+}
+
+// <summary> This method cancels sender
+void senderCancel() {
+    pthread_cancel(SenderThread);
+}
+
+
+// <summary> This method initializes sender
+void senderInit(char* HostnameArg, char* PortArg, List* ListArg) {
+    Hostname = HostnameArg;
+    Port = PortArg;
+    LList = ListArg;
+
+    int StVal = pthread_create(&SenderThread, NULL, SenderUnload, NULL);
+    if (StVal != 0) {
+        perror("sender: thread creation error");
+        exit(-1);
+    }
+}
+
+// <summary> This method frees all allocated messages and closes the sender thread
+void senderShutdown() {
+    free(Message);
+    Message = NULL;
 
     freeaddrinfo(ServInfo);
-}
 
+    close(SocketEndPoint);
 
-// <summary> Closes the socket file descriptor...
-void CloseUDPSender()
-{
-    close(SocketEndpointValue);
-}
-
-
-// ---- TODO: write some tests to verify if -----
-// 1.) The sender is reading real hostnames and port numbers
-// 2.) If the sender is transmitting bytes
-// 3.) The bytes are being sent to the correct location
-// *4.) When testing is done, complete the header by defining SetupUDPSender and CloseUDPSender
-int main() 
-{
-    printf("Welcome\n");
-    // Sample input
-    char* hostname = "asb9700u-g06"; // Replace with your target hostname
-    char* port = "999999999999999999"; // Replace with the target port
-    List* messageList = List_create();
-    
-    
-    // Sample messages
-    List_insert_after(messageList, "Hello, world!");
-    List_insert_after(messageList, "How are you?");
-    List_insert_after(messageList, "Goodbye!");
-    List_insert_after(messageList, "Yes!");
-    List_insert_after(messageList, "No!");
-    List_insert_after(messageList, "What!");
-    
-    // Initialize the sender
-    SetupUDPSender(hostname, port, messageList);
-
-    // Simulate sending messages for a few seconds
-    sleep(3); // Adjust the duration as needed
-
-    // Shutdown the sender
-    CloseUDPSender();
-
-    // Free the message list
-    List_free(messageList, NULL);
-    
-
-    return 0;
+    pthread_join(SenderThread, NULL);
 }
--- transmit.h
+++ transmit.h
@@ -1,17 +1,11 @@
 #ifndef TRANSMIT_H
 #define TRANSMIT_H
 
-#include "list.h"
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+void senderInit(char* HostnameArg, char* PortArg, List* ListArg);
+void senderCancel();
+void senderShutdown();
+void senderSignaller();
 
-// Functions
-struct addrinfo SetUpHints(struct addrinfo HintsArg);
-int SetUpSocket(int SocketArg);
-void SetupUDPSender(char* hostname, char* port, List* list);
-void CloseUDPSender();
 
 #endif
 
